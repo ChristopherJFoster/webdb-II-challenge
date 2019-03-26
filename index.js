3e8c02d66c57fd81b9ab027b356f80b6fd60069c
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-26 17:40:08 -0400

    Stretch complete. Copied and refactored zoos endpoints to be bears endpoints.

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+// zoos endpoints:
+
 server.post('/api/zoos', (req, res) => {
   const { name } = req.body;
   if (!name) {
@@ -114,6 +116,107 @@ server.put('/api/zoos/:id', (req, res) => {
         res
           .status(500)
           .json({ error: `There was an error updating the zoo data. ${err}` })
+      );
+  }
+});
+
+// bears endpoints:
+
+server.post('/api/bears', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res
+      .status(400)
+      .json({ error: 'You must provide a name for the new bear record.' });
+  } else {
+    db('bears')
+      .insert({ name })
+      .then(ids => {
+        const id = ids[0];
+        db('bears')
+          .where({ id })
+          .first()
+          .then(bear => {
+            res.status(200).json(bear);
+          });
+      })
+      .catch(err =>
+        res.status(500).json({
+          error: `There was an error adding the bear data, or an error retrieving the added bear data. ${err}`
+        })
+      );
+  }
+});
+
+server.get('/api/bears', (req, res) => {
+  db('bears')
+    .then(bears => {
+      res.status(200).json(bears);
+    })
+    .catch(err =>
+      res.status(500).json({
+        error: `There was an error retrieving the bears data. ${err}`
+      })
+    );
+});
+
+server.get('/api/bears/:id', (req, res) => {
+  db('bears')
+    .where('id', req.params.id)
+    .first()
+    .then(bear => {
+      if (bear) {
+        res.status(200).json(bear);
+      } else {
+        res.status(404).json({ error: 'There is no bear with that ID.' });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: `There was an error retrieving the bear data. ${err}` })
+    );
+});
+
+server.delete('/api/bears/:id', (req, res) => {
+  db('bears')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'Bear data successfully deleted.' });
+      } else {
+        res.status(404).json({ error: 'There is no bear with that ID.' });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: `There was an error deleting the bear data. ${err}` })
+    );
+});
+
+server.put('/api/bears/:id', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({
+      error: 'Please provide the changes you intend to make to the bear record.'
+    });
+  } else {
+    db('bears')
+      .where({ id: req.params.id })
+      .update(req.body)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json({ message: 'Bear data successfully updated.' });
+        } else {
+          res.status(404).json({ error: 'There is no bear with that ID.' });
+        }
+      })
+      .catch(err =>
+        res
+          .status(500)
+          .json({ error: `There was an error updating the bear data. ${err}` })
       );
   }
 });
